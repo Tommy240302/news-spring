b858f54d520f6818d229736a85837f71ae51b872
Minh Quang <laikhacminhquang24032002@gmail.com>
2025-08-01 15:34:23 +0700

    Get Category

--- src/main/java/com/ptit/news/controller/UserController.java
+++ src/main/java/com/ptit/news/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.ptit.news.controller;
 
-import org.axonframework.queryhandling.QueryGateway;
-import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.springframework.web.bind.annotation.*;
 import com.ptit.news.query.dto.GetUserByIdQuery;
 import com.ptit.news.dto.UserResponse;
@@ -14,14 +12,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController extends BaseController {
 
-    private final QueryGateway queryGateway;
-
     @GetMapping("/{id}")
     public Response<UserResponse> getUserById(@PathVariable Long id) {
-        GetUserByIdQuery query = GetUserByIdQuery.builder()
-                .id(id)
-                .build();
-        return executeQuery(query, UserResponse.class); // Dùng hàm type-safe
+        GetUserByIdQuery query = GetUserByIdQuery.builder().id(id).build();
+        return executeQuery(query, UserResponse.class);
     }
-
 }
--- src/main/java/com/ptit/news/query/dto/GetAllCategoryQuery.java
+++ src/main/java/com/ptit/news/query/dto/GetAllCategoryQuery.java
@@ -1,4 +1,9 @@
 package com.ptit.news.query.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class GetAllCategoryQuery {
 }
