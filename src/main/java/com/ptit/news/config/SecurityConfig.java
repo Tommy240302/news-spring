e536c8860276d519379b1ab5da9b7abba50372d1
Minh Quang <laikhacminhquang24032002@gmail.com>
2025-08-11 08:45:12 +0700

    se cf

--- src/main/java/com/ptit/news/config/SecurityConfig.java
+++ src/main/java/com/ptit/news/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     @Bean
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // hoặc "*", nếu không bảo mật
+    config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174")); // hoặc "*", nếu không bảo mật
     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(Arrays.asList("*"));
     config.setAllowCredentials(true); // nếu bạn dùng cookies/session
