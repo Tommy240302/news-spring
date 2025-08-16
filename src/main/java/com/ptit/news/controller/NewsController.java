e2f4ce95572e4914ddf6e986e3c9ab9f237bfc1f
Minh Quang <laikhacminhquang24032002@gmail.com>
2025-08-16 15:08:18 +0700

    controller my-news

--- src/main/java/com/ptit/news/controller/NewsController.java
+++ src/main/java/com/ptit/news/controller/NewsController.java
@@ -4,6 +4,7 @@ import com.ptit.news.command.dto.CountViewCommand;
 import com.ptit.news.common.Response;
 import com.ptit.news.dto.CommentResponse;
 import com.ptit.news.dto.NewsResponse;
+import com.ptit.news.entity.News;
 import com.ptit.news.query.dto.GetAllCommentApprovedByNewsId;
 import com.ptit.news.query.dto.GetAllPostQuery;
 import com.ptit.news.query.dto.GetPostByIdQuery;
@@ -11,6 +12,11 @@ import lombok.RequiredArgsConstructor;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.ptit.news.repository.UserRepository;
+import com.ptit.news.repository.NewsRepository;
+import com.ptit.news.entity.User;
+
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -18,6 +24,10 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RequiredArgsConstructor
 public class NewsController extends AdvancedBaseController {
+
+    private final UserRepository userRepository;
+    private final NewsRepository newsRepository;
+    
     @GetMapping
     public Response<List<NewsResponse>> getAllNews() {
         List<NewsResponse> news = queryGateway.query(
@@ -49,4 +59,18 @@ public class NewsController extends AdvancedBaseController {
     public Response<String> addView(@RequestBody CountViewCommand command) {
         return executeCommand(command);
     }
+
+    @GetMapping("/my-news")
+    public Response<List<NewsResponse>> getMyNews(Principal principal) {
+        String email = principal.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Người dùng không tồn tại"));
+        List<NewsResponse> myNews = newsRepository.findByAuthor(user).stream()
+                .filter(news -> !news.getIsDeleted())
+                .map(NewsResponse::new)
+                .toList();
+        return Response.Success(myNews, "Lấy bài viết của bạn thành công");
+                
+    }
+
 }
